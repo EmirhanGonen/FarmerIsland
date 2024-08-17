77c7cfaaf0ecfcc4bd5a821bd82570ffa88156d1
Emirhan Gönen <emirhangnn0607@gmail.com>
2024-08-17 14:31:38 +0300

    Polish End

--- FarmerIsland/Assets/Scripts/UI/LevelUpPopup.cs
+++ FarmerIsland/Assets/Scripts/UI/LevelUpPopup.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,25 +11,31 @@ public class LevelUpPopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _oldIslandSize = null;
     [SerializeField] private TextMeshProUGUI _newIslandSize = null;
 
+    [SerializeField] private TextMeshProUGUI _oldIslandCapacityCountText = null;
+    [SerializeField] private TextMeshProUGUI _newIslandCapacityCountText = null;
 
     private FruitBushDataList _dataList = null;
     private const string DATA_LIST_PATH = "Datas/FruitBushDataList";
 
     public static System.Action OnClosedPopup = null;
 
-    private void Awake() {
+    private void Awake()
+    {
         _dataList = Resources.Load<FruitBushDataList>(DATA_LIST_PATH);
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         PlayerLevel.OnPlayerLevelUp += InitializePopup;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         PlayerLevel.OnPlayerLevelUp -= InitializePopup;
     }
 
-    private void InitializePopup(int Level) {
+    private void InitializePopup(int Level)
+    {
         if (Level.Equals(1))
             return;
 
@@ -37,11 +44,15 @@ public class LevelUpPopup : MonoBehaviour
         _newFruitImage.sprite = NewFruit.FruitSprite;
         _newFruitName.SetText(NewFruit.FruitName);
 
-        _oldIslandSize.SetText((Island.Instance.Scale.x).ToString());
-        _newIslandSize.SetText(Island.Instance.GetNewIslandSize(Level).ToString());
+       // _oldIslandSize.SetText((Island.Instance.Scale.x).ToString(CultureInfo.InvariantCulture));
+       // _newIslandSize.SetText(Island.Instance.GetNewIslandSize(Level).ToString());
+
+        _oldIslandCapacityCountText.SetText(FruitBushSpawner.Instance.GetCapacityByLevel(Level - 1).ToString());
+        _newIslandCapacityCountText.SetText(FruitBushSpawner.Instance.GetCapacityByLevel(Level).ToString());
 
         transform.GetChild(0).gameObject.SetActive(true);
     }
+
     public void ClosePopup()
     {
         OnClosedPopup?.Invoke();
